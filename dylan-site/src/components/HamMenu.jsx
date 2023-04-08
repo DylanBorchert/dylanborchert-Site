fa62b1b5ecddfa29871e0d3a70f2751f140cdead
Dylan Borchert <dborc610@mtroyal.ca>
2023-04-08 10:15:00 -0600

    removed unused footer

--- dylan-site/src/components/HamMenu.jsx
+++ dylan-site/src/components/HamMenu.jsx
@@ -9,34 +9,6 @@ import ContactCard from "./ContactCard.jsx";
 
 function HamMenu(props) {
 
-    // useEffect(() => {
-    //     axios
-    //     .get(`https://api.github.com/repos/dborc610/dylanborchert-Site/commits`)
-    //       .then((response) => {
-    //         const commitSha = response.data[0]?.sha;
-    //         const commitMessage = response.data[0]?.commit.message;
-    //         let commitTime = response.data[0].commit.committer.date
-    //           .replace("T", " at ")
-    //           .replace("Z", " ");
-    
-    //         let splittedTime = commitTime.split(" at ");
-    //         let time = splittedTime[1].split(":");
-    //         let hour = parseInt(time[0]);
-    //         time[0] = hour - 7 < 0 ? hour + 17 : hour - 7;
-    //         time = time.join(":");
-    //         splittedTime[1] = time;
-    //         commitTime = splittedTime.join(" at ");
-    
-    //         setCommit({
-    //           sha: commitSha,
-    //           time: commitTime,
-    //           message: commitMessage,
-    //         });
-    //       })
-    //       .catch((error) => {
-    //         console.log(error);
-    //       });
-    //   });
 
     return(
         <div className="absolute w-full z-10 bg-black flex flex-col align-middle justify-around h-[100dvh]">
