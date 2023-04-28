0c3972d8701cc99e7f5362eaded7f5241baac2ba
Dylan Borchert <dborc610@mtroyal.ca>
2023-04-28 12:14:30 -0600

    fixed resume downloads. Also changed home page

--- dylan-site/src/pages/index.tsx
+++ dylan-site/src/pages/index.tsx
@@ -2,24 +2,17 @@ import React from 'react'
 import VantaNet from '../animated-components/VantaNet'
 import VantaBirds from '../animated-components/VantaBirds'
 import Navbar from '../components/Navbar'
+import Particals from '../animated-components/DotParticals'
 
 
 export default function Home({commit_msg}: any) {
   return (
     <div className="w-[100%] h-[100dvh]">
-        <VantaNet styles={"fixed top-0 -z-10"}/>
+        <Particals styles={"fixed top-0 -z-10"}/>
       <div className="flex flex-col justify-between h-[100dvh]">
         <Navbar commit_msg={commit_msg}/>   
-        <div className=" text-center font-semibold text-white"> 
-            {/* <Typewriter
-            options={{
-                strings: ['Under Construction', 'Coming Soon', 'Check Back Later'],
-                autoStart: true,
-                loop: true,
-                deleteSpeed: 30,
-                delay: 80,
-            }}
-            /> */}
+        <div className=" text-center font-semibold text-white h-96"> 
+          
         </div>
         <div></div>
       </div>
