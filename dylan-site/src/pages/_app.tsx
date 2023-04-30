9df4ba14743b405648343596b33a7dfac06afa4c
Dylan Borchert <dborc610@mtroyal.ca>
2023-04-30 08:30:02 -0600

    file directory change for deployment

--- dylan-site/src/pages/_app.tsx
+++ dylan-site/src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '@/styles/globals.css'
-import '@/styles/slidetextlarge.css';
-import '@/styles/slidetextsmall.css';
-import '@/styles/slidetext.css';
-import '@/styles/fliptext.css';
-import '@/styles/markdown.css';
+import '../styles/slidetextlarge.css';
+import '../styles/slidetextsmall.css';
+import '../styles/slidetext.css';
+import '../styles/fliptext.css';
+import '../styles/markdown.css';
 
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
