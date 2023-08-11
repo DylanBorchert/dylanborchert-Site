5002852f1f3904646963829b2cb71166e9e3a6a0
Dylan Borchert <dborc610@mtroyal.ca>
2023-08-11 14:25:53 -0600

    made changes to contentProvider for offline mode

--- dylan-site/src/modules/ContentProvider.js
+++ dylan-site/src/modules/ContentProvider.js
@@ -22,7 +22,34 @@ export const getHomePage = async function () {
 		fs.writeFile('/home-page.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
 		return [response.data];
 	} else {
+		console.log("error loading home page");
 		const data = fs.readFileSync('/home-page.json', 'utf8');
+		return [JSON.parse(data)];
+	}
+};
+
+export const getAboutPage = async function () {
+	axios.defaults.headers.common[
+		"Authorization"
+	] = `bearer ${process.env.STRAPI_KEY}`;
+
+	const response = await axios.get(
+		`${process.env.STRAPI_HOST}/api/about-page`,
+		{
+			params: {
+				"populate[content][populate][projects][populate]": "cover, tags",
+				"populate[content][populate][blogs][populate]": "cover, tags",
+			},
+		}
+	).catch((err) => {
+		console.log("error loading about page");
+	});
+	if (response?.data) {
+		fs.writeFile('/about-page.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
+		return [response.data];
+	} else {
+		console.log("error loading about page");
+		const data = fs.readFileSync('/about-page.json', 'utf8');
 		return [JSON.parse(data)];
 	}
 };
@@ -48,6 +75,7 @@ export const getProjectPage = async function () {
 		fs.writeFile('/project-page.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
 		return [response.data];
 	} else {
+		console.log("error loading project page");
 		const data = fs.readFileSync('/project-page.json', 'utf8');
 		return [JSON.parse(data)];
 	}
@@ -74,7 +102,60 @@ export const getBlogPage = async function () {
 		fs.writeFile('/blog-page.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
 		return [response.data];
 	} else {
+		console.log("error loading blog page");
 		const data = fs.readFileSync('/blog-page.json', 'utf8');
+		return [JSON.parse(data)];
+	}
+};
+
+export const getAllProjects = async function () {
+	axios.defaults.headers.common[
+		"Authorization"
+	] = `bearer ${process.env.STRAPI_KEY}`;
+	
+	const response = await axios.get(
+		`${process.env.STRAPI_HOST}/api/projects`,
+		{
+			params: {
+				"populate[content][populate][projects][populate]": "cover, tags",
+				"populate[content][populate][blogs][populate]": "cover, tags",
+			},
+		}
+	).catch((err) => {
+		console.log("error loading projects");
+	});
+	if (response?.data) {
+		fs.writeFile('/projects.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
+		return [response.data];
+	} else {
+		console.log("error loading projects");
+		const data = fs.readFileSync('/projects.json', 'utf8');
+		return [JSON.parse(data)];
+	}
+};
+
+export const getAllBlogs = async function () {
+	axios.defaults.headers.common[
+		"Authorization"
+	] = `bearer ${process.env.STRAPI_KEY}`;
+
+	const response = await axios.get(
+		`${process.env.STRAPI_HOST}/api/blogs`,
+		{
+			params: {
+				"populate[content][populate][projects][populate]": "cover, tags",
+				"populate[content][populate][blogs][populate]": "cover, tags",
+			},
+		}
+	).catch((err) => {
+		console.log("error loading blogs");
+	});
+	if (response?.data) {
+		fs.writeFile('/blogs.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
+		return [response.data];
+	} else {
+		console.log("error loading blogs");
+		const data = fs.readFileSync('/blogs.json', 'utf8');
 		return [JSON.parse(data)];
 	}
 };
@@ -99,10 +180,11 @@ export const getProject = async function (UID) {
 		console.log("error loading project");
 	});
 	if (response?.data) {
-		fs.writeFile(`/projects/${UID}.json`, JSON.stringify(response.data), (err) => { if (err) console.log(err) });
+		fs.writeFile(`/projects-${UID}.json`, JSON.stringify(response.data), (err) => { if (err) console.log(err) });
 		return [response.data];
 	} else {
-		const data = fs.readFileSync(`/projects/${UID}.json`, 'utf8');
+		console.log("error loading project");
+		const data = fs.readFileSync(`/projects-${UID}.json`, 'utf8');
 		return [JSON.parse(data)];
 	}
 };
@@ -124,10 +206,11 @@ export const getBlog = async function (UID) {
 		console.log("error loading blog");
 	});
 	if (response?.data) {
-		fs.writeFile(`/blogs/${UID}.json`, JSON.stringify(response.data), (err) => { if (err) console.log(err) });
+		fs.writeFile(`/blogs-${UID}.json`, JSON.stringify(response.data), (err) => { if (err) console.log(err) });
 		return [response.data];
 	} else {
-		const data = fs.readFileSync(`/blogs/${UID}.json`, 'utf8');
+		console.log("error loading blog");
+		const data = fs.readFileSync(`/blogs-${UID}.json`, 'utf8');
 		return [JSON.parse(data)];
 	}
 };
