34943a3b27a5165d529519ac0a311b4d25879baf
Dylan Borchert <dborc610@mtroyal.ca>
2023-08-20 10:40:22 -0600

    content provide refactor and toast fix

--- dylan-site/src/modules/ContentProvider.js
+++ dylan-site/src/modules/ContentProvider.js
@@ -1,251 +1,122 @@
 import axios from "axios";
 import fs from "fs";
 
+const axiosWithAuthorization = axios.create({
+	headers: {
+		Authorization: `bearer ${process.env.STRAPI_KEY}`,
+	},
+});
 
-export const getHomePage = async function () {
-	axios.defaults.headers.common[
-		"Authorization"
-	] = `bearer ${process.env.STRAPI_KEY}`;
-
-	const response = await axios.get(
-		`${process.env.STRAPI_HOST}/api/home-page`,
-		{
-			params: {
-				"populate[content][populate][projects][populate]": "cover, tags",
-				"populate[content][populate][blogs][populate]": "cover, tags",
-			},
-		}
-	).catch((err) => {
-		console.log("error loading home page");
-	});
-	if (response?.data) {
-		fs.writeFile('/home-page.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
-		return [response.data];
-	} else {
-		console.log("error loading home page");
-		// check if file exits first
-		if(fs.existsSync('/home-page.json')) { 
-			const data = fs.readFileSync('/home-page.json', 'utf8');
-			return [JSON.parse(data)];
-		} else {
-			return null;
-		}
+const fetchData = async (url, params) => {
+	try {
+		const response = await axiosWithAuthorization.get(url, params);
+		return response.data ? [response.data] : null;
+	} catch (error) {
+		console.log("error loading data:", error);
+		return null;
 	}
 };
 
-export const getAboutPage = async function () {
-	axios.defaults.headers.common[
-		"Authorization"
-	] = `bearer ${process.env.STRAPI_KEY}`;
+export const getHomePage = async () => {
+	const url = `${process.env.STRAPI_HOST}/api/home-page`;
+	const params = {
+		params: {
+			"populate[content][populate][projects][populate]": "cover, tags",
+			"populate[content][populate][blogs][populate]": "cover, tags",
+		},
+	};
 
-	const response = await axios.get(
-		`${process.env.STRAPI_HOST}/api/about-page`,
-		{
-			params: {
-				"populate[content][populate][projects][populate]": "cover, tags",
-				"populate[content][populate][blogs][populate]": "cover, tags",
-			},
-		}
-	).catch((err) => {
-		console.log("error loading about page");
-	});
-	if (response?.data) {
-		fs.writeFile('/about-page.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
-		return [response.data];
-	} else {
-		console.log("error loading about page");
-		if(fs.existsSync('/about-page.json')) {
-			const data = fs.readFileSync('/about-page.json', 'utf8');
-			return [JSON.parse(data)];
-		} else {
-			return null;
-		}
-	}
+	return await fetchData(url, params);
 };
 
+export const getAboutPage = async () => {
+	const url = `${process.env.STRAPI_HOST}/api/about-page`;
+	const params = {
+		params: {
+			"populate[content][populate][projects][populate]": "cover, tags",
+			"populate[content][populate][blogs][populate]": "cover, tags",
+		},
+	};
 
-export const getProjectPage = async function () {
-	axios.defaults.headers.common[
-		"Authorization"
-	] = `bearer ${process.env.STRAPI_KEY}`;
-
-	const response = await axios.get(
-		`${process.env.STRAPI_HOST}/api/projects-page`,
-		{
-			params: {
-				"populate[content][populate][projects][populate]": "cover, tags",
-				"populate[content][populate][blogs][populate]": "cover, tags",
-			},
-		}
-	).catch((err) => {
-		console.log("error loading project page");
-	});
-	if (response?.data) {
-		fs.writeFile('/project-page.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
-		return [response.data];
-	} else {
-		console.log("error loading project page");
-		if(fs.existsSync('/project-page.json')) {
-			const data = fs.readFileSync('/project-page.json', 'utf8');
-			return [JSON.parse(data)];
-		} else {
-			return null;
-		}
-	}
+	return await fetchData(url, params);
 };
 
+export const getProjectPage = async () => {
+	const url = `${process.env.STRAPI_HOST}/api/projects-page`;
+	const params = {
+		params: {
+			"populate[content][populate][projects][populate]": "cover, tags",
+			"populate[content][populate][blogs][populate]": "cover, tags",
+		},
+	};
 
-export const getBlogPage = async function () {
-	axios.defaults.headers.common[
-		"Authorization"
-	] = `bearer ${process.env.STRAPI_KEY}`;
-
-	const response = await axios.get(
-		`${process.env.STRAPI_HOST}/api/blogs-page`,
-		{
-			params: {
-				"populate[content][populate][projects][populate]": "cover, tags",
-				"populate[content][populate][blogs][populate]": "cover, tags",
-			},
-		}
-	).catch((err) => {
-		console.log("error loading blog page");
-	});
-	if (response?.data) {
-		fs.writeFile('/blog-page.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
-		return [response.data];
-	} else {
-		console.log("error loading blog page");
-		if(fs.existsSync('/blog-page.json')) {
-			const data = fs.readFileSync('/blog-page.json', 'utf8');
-			return [JSON.parse(data)];
-		} else {
-			return null;
-		}
-	}
+	return await fetchData(url, params);
 };
 
-export const getAllProjects = async function () {
-	axios.defaults.headers.common[
-		"Authorization"
-	] = `bearer ${process.env.STRAPI_KEY}`;
-	
-	const response = await axios.get(
-		`${process.env.STRAPI_HOST}/api/projects`,
-		{
-			params: {
-				"populate[content][populate][projects][populate]": "cover, tags",
-				"populate[content][populate][blogs][populate]": "cover, tags",
-			},
-		}
-	).catch((err) => {
-		console.log("error loading projects");
-	});
-	if (response?.data) {
-		fs.writeFile('/projects.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
-		return [response.data];
-	} else {
-		console.log("error loading projects");
-		if(fs.existsSync('/projects.json')) {
-			const data = fs.readFileSync('/projects.json', 'utf8');
-			return [JSON.parse(data)];
-		} else {
-			return null;
-		}
-	}
+export const getBlogPage = async () => {
+	const url = `${process.env.STRAPI_HOST}/api/blogs-page`;
+	const params = {
+		params: {
+			"populate[content][populate][projects][populate]": "cover, tags",
+			"populate[content][populate][blogs][populate]": "cover, tags",
+		},
+	};
+
+	return await fetchData(url, params);
 };
 
-export const getAllBlogs = async function () {
-	axios.defaults.headers.common[
-		"Authorization"
-	] = `bearer ${process.env.STRAPI_KEY}`;
+export const getAllProjects = async () => {
+	const url = `${process.env.STRAPI_HOST}/api/projects`;
+	const params = {
+		params: {
+			"populate[content][populate][projects][populate]": "cover, tags",
+			"populate[content][populate][blogs][populate]": "cover, tags",
+		},
+	};
 
-	const response = await axios.get(
-		`${process.env.STRAPI_HOST}/api/blogs`,
-		{
-			params: {
-				"populate[content][populate][projects][populate]": "cover, tags",
-				"populate[content][populate][blogs][populate]": "cover, tags",
-			},
-		}
-	).catch((err) => {
-		console.log("error loading blogs");
-	});
-	if (response?.data) {
-		fs.writeFile('/blogs.json', JSON.stringify(response.data), (err) => { if (err) console.log(err) });
-		return [response.data];
-	} else {
-		console.log("error loading blogs");
-		if(fs.existsSync('/blogs.json')) {
-			const data = fs.readFileSync('/blogs.json', 'utf8');
-			return [JSON.parse(data)];
-		} else {
-			return null;
-		}
-	}
+	return await fetchData(url, params);
 };
 
+export const getAllBlogs = async () => {
+	const url = `${process.env.STRAPI_HOST}/api/blogs`;
+	const params = {
+		params: {
+			"populate[content][populate][projects][populate]": "cover, tags",
+			"populate[content][populate][blogs][populate]": "cover, tags",
+		},
+	};
 
-
-
-export const getProject = async function (UID) {
-	axios.defaults.headers.common[
-		"Authorization"
-	] = `bearer ${process.env.STRAPI_KEY}`;
-
-	const response = await axios.get(
-		`${process.env.STRAPI_HOST}/api/projects`,
-		{
-			params: {
-				"filters[UID][$eq]": UID,
-				"populate[content][populate][projects][populate]": "cover, tags"
-			},
-		}
-	).catch((err) => {
-		console.log("error loading project");
-	});
-	if (response?.data) {
-		fs.writeFile(`/projects-${UID}.json`, JSON.stringify(response.data), (err) => { if (err) console.log(err) });
-		return [response.data];
-	} else {
-		console.log("error loading project");
-		if(fs.existsSync(`/projects-${UID}.json`)) {
-			const data = fs.readFileSync(`/projects-${UID}.json`, 'utf8');
-			return [JSON.parse(data)];
-		} else {
-			return null;
-		}
-	}
+	return await fetchData(url, params);
 };
 
-export const getBlog = async function (UID) {
-	axios.defaults.headers.common[
-		"Authorization"
-	] = `bearer ${process.env.STRAPI_KEY}`;
+export const getProject = async (UID) => {
+	const url = `${process.env.STRAPI_HOST}/api/projects`;
+	const params = {
+		params: {
+			"filters[UID][$eq]": UID,
+			"populate[content][populate][projects][populate]": "cover, tags",
+		},
+	};
 
-	const response = await axios.get(
-		`${process.env.STRAPI_HOST}/api/blogs`,
-		{
-			params: {
-				"filters[UID][$eq]": UID,
-				"populate[content][populate][blogs][populate]": "cover, tags"
-			},
-		}
-	).catch((err) => {
-		console.log("error loading blog");
-	});
-	if (response?.data) {
-		fs.writeFile(`/blogs-${UID}.json`, JSON.stringify(response.data), (err) => { if (err) console.log(err) });
-		return [response.data];
-	} else {
-		console.log("error loading blog");
-		if(fs.existsSync(`/blogs-${UID}.json`)) {
-			const data = fs.readFileSync(`/blogs-${UID}.json`, 'utf8');
-			return [JSON.parse(data)];
-		} else {
-			return null;
-		}
-	}
+	return await fetchData(url, params);
 };
 
-export default { getHomePage, getProjectPage, getBlogPage, getProject, getBlog };
+export const getBlog = async (UID) => {
+	const url = `${process.env.STRAPI_HOST}/api/blogs`;
+	const params = {
+		params: {
+			"filters[UID][$eq]": UID,
+			"populate[content][populate][blogs][populate]": "cover, tags",
+		},
+	};
+
+	return await fetchData(url, params);
+};
+
+export default {
+	getHomePage,
+	getProjectPage,
+	getBlogPage,
+	getProject,
+	getBlog,
+};
