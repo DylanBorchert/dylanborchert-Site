1c65371819bb937e2f97d88d3b7324fd993ae10e
Dylan Borchert <dborc610@mtroyal.ca>
2023-09-07 07:45:39 -0600

    added about page

---
+++ dylan-site/src/components/About.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar.jsx";
+import { useEffect, useState } from "react";
+import Typewriter from 'typewriter-effect';
+import ContentProcessor from "./ContentProcessor.jsx";
+import SocialsCard from "./SocialsCard.jsx";
+
+function About(props:any ) {
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default About;
